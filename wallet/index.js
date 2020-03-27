fc5a0bb78ec68795d3dec6fa9440fd4da4610a36
Thomas Dekeyser <tmsdekeyser@gmail.com>
2020-03-27 22:00:22 +0100

    Complete wallet directory pull

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const { STARTING_BALANCE } = require('../config');
 const CryptoUtil = require('../crypto-util');
+const Transaction = require('./transaction');
 
 class Wallet {
     constructor() {
@@ -14,6 +15,70 @@ class Wallet {
             Balance: ${this.balance}`
         );
     }
+
+    sign(dataHash) {
+        return this.keyPair.sign(dataHash);
+      }
+    
+      createTransaction(recipient, amount, blockchain, transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
+    
+        if (amount > this.balance) {
+          console.log(`Amount: ${amount} exceceds current balance: ${this.balance}`);
+          return;
+        }
+    
+        let transaction = transactionPool.existingTransaction(this.publicKey);
+    
+        if (transaction) {
+          transaction.update(this, recipient, amount);
+        } else {
+          transaction = Transaction.newTransaction(this, recipient, amount);
+          transactionPool.updateOrAddTransaction(transaction);
+        }
+    
+        return transaction;
+      }
+    
+      calculateBalance(blockchain) {
+        let balance = this.balance;
+        let transactions = [];
+        blockchain.chain.forEach(block => block.data.forEach(transaction => {
+          transactions.push(transaction);
+        }));
+    
+        const walletInputTs = transactions
+          .filter(transaction => transaction.input.address === this.publicKey);
+    
+        let startTime = 0;
+    
+        if (walletInputTs.length > 0) {
+          const recentInputT = walletInputTs.reduce(
+            (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+          );
+    
+          balance = recentInputT.outputs.find(output => output.address === this.publicKey).amount;
+          startTime = recentInputT.input.timestamp;
+        }
+    
+        transactions.forEach(transaction => {
+          if (transaction.input.timestamp > startTime) {
+            transaction.outputs.find(output => {
+              if (output.address === this.publicKey) {
+                balance += output.amount;
+              }
+            });
+          }
+        });
+    
+        return balance;
+      }
+    
+      static blockchainWallet() {
+        const blockchainWallet = new this();
+        blockchainWallet.address = 'blockchain-wallet';
+        return blockchainWallet;
+      }
 }
 
 module.exports = Wallet;
