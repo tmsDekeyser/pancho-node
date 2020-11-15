f64f75a7e160990a6048086e1134550be3895f0e
Thomas Dekeyser <tmsdekeyser@gmail.com>
2020-11-15 17:59:26 +0100

    Untrack secretKey.js

--- app/secretKey.js
+++
@@ -1,7 +0,0 @@
-const keySet = {
-    publishKey: 'pub-c-3ae6dace-fc08-4626-b254-d35955b15049',
-    subscribeKey: 'sub-c-4cd913a4-74bc-11ea-b179-9eded212fb8b',
-    secretKey: 'sec-c-MWJkNjFkZTQtYWQwNS00MDMzLWFlMGItZjk3MzU2NDk5MzNk'
-}
-
-module.exports = { keySet };
