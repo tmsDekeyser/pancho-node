5eaa0439d619c698b8b4d9557a7eb821203f966c
Thomas Dekeyser <tmsdekeyser@gmail.com>
2020-11-15 17:50:51 +0100

    Add PubNub keys to secrets file

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,11 +1,7 @@
 const PubNub = require('pubnub');
 const Wallet = require('../wallet');
+const keySet = require('./secretKey');
 
-const keySet = {
-    publishKey: 'pub-c-3ae6dace-fc08-4626-b254-d35955b15049',
-    subscribeKey: 'sub-c-4cd913a4-74bc-11ea-b179-9eded212fb8b',
-    secretKey: 'sec-c-MWJkNjFkZTQtYWQwNS00MDMzLWFlMGItZjk3MzU2NDk5MzNk'
-}
 
 const CHANNELS = {
     TEST: 'TEST',
